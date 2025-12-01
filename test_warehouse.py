e025e5e7ec207cf16e2f64daace204da07e6a16d
Kadyr <Nekketsu4@mail.ru>
2025-12-01 18:36:49 +0300

    fixed: fixed  test_error_subtract_qty_product_more_than_exist

--- test_warehouse.py
+++ test_warehouse.py
@@ -1,23 +1,12 @@
+import pytest
+
 from model import Product, WareHouse
 
 
-# def test_allocate_product_to_warehouse():
-#     warehouse, product = create_product_and_warehouse(
-#         'warehouse',
-#         'product-1', 'Table', 5
-#     )
-#     warehouse.allocate(product)
-#     assert warehouse.available_qty == 5
-#
-# def test_deallocate_product_to_warehouse():
-#     warehouse, product = create_product_and_warehouse(
-#         'warehouse',
-#         'product-1', 'Table', 2
-#     )
-#     warehouse.deallocate(product)
-#     assert warehouse.available_qty == 3
-
 def test_if_exist_product_in_catalog():
+    """
+    Тест что товар есть в каталоге
+    """
     catalog = WareHouse('warehouse')
     product = Product('product', 'TABLE', 20)
 
@@ -25,6 +14,10 @@ def test_if_exist_product_in_catalog():
     assert catalog.has_product_in_catalog(product)
 
 def test_can_add_qty_of_product():
+    """
+    Тест что можем добавить некоторое количество товаров
+    на склад
+    """
     catalog = WareHouse('warehouse')
     product = Product('product', 'TABLE', 20)
 
@@ -32,6 +25,10 @@ def test_can_add_qty_of_product():
     assert catalog.products[product.article]['qty'] == 20
 
 def test_can_add_many_times_qty_of_product():
+    """
+    Тест что можем добавить некоторое количество товаров
+    несколько раз на склад
+    """
     catalog = WareHouse('warehouse')
     product = Product('product', 'TABLE', 20)
     product1 = Product('product', 'TABLE', 50)
@@ -41,6 +38,9 @@ def test_can_add_many_times_qty_of_product():
     assert catalog.products[product.article]['qty'] == 70
 
 def test_subtract_qty_of_product_from_warehouse():
+    """
+    Тесч что можно вычесть некоторое количество товаров из склада
+    """
     catalog = WareHouse('warehouse')
     product = Product('product', 'TABLE', 20)
     catalog.add_product(product)
@@ -49,11 +49,16 @@ def test_subtract_qty_of_product_from_warehouse():
     catalog.subtract_product(product1)
     assert catalog.products[product.article]['qty'] == 7
 
-def test_can_subtract_qty_of_product_from_warehouse():
+def test_error_subtract_qty_product_more_than_exist():
+    """
+    Тест вызывает исключение если запрашиваемое количество товара
+    больше чем то, что есть на складе
+    """
     catalog = WareHouse('warehouse')
     product = Product('product', 'TABLE', 20)
     catalog.add_product(product)
 
     product1 = Product('product', 'TABLE', 25)
-    catalog.subtract_product(product1)
-    assert catalog.can_subtract(product1) is False
+
+    with pytest.raises(ValueError, match='На складе едостаточно товара: TABLE'):
+        catalog.subtract_product(product1)
