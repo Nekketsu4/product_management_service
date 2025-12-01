442e82f52f549ae5bf84cfa4fbe0384e5a11be0b
Kadyr <Nekketsu4@mail.ru>
2025-12-01 17:12:27 +0300

    fixed: model.py and tests/test_allocate_goods.py

--- model.py
+++ model.py
@@ -1,26 +1,35 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import datetime
 
 
-@dataclass(unsafe_hash=True)
+@dataclass
 class Product:
     article: str
     name: str
     qty: int
 
 
-
 class WareHouse:
     def __init__(self, name):
         self.name = name
-        self._products = list()
+        self.products = dict()
         self.time_of_action = datetime.datetime
-        self.available_qty = 0
 
-    def allocate(self, product: Product):
-        self.available_qty += product.qty
+    def has_product_in_catalog(self, product: Product):
+        return product.article in self.products
 
-    def deallocate(self, product: Product):
-        self.available_qty -= product.qty
+    def can_subtract(self, product: Product):
+        return (self.has_product_in_catalog(product)
+                and self.products[product.article]['qty'] - product.qty >= 0)
+
+    def add_product(self, product: Product):
+        if not self.has_product_in_catalog(product):
+            self.products.update({product.article: asdict(product)})
+        else:
+            self.products[product.article]['qty'] += product.qty
+
+    def subtract_product(self, product: Product):
+        if self.can_subtract(product):
+            self.products[product.article]['qty'] -= product.qty
 
--- test_allocate_goods.py
+++ test_allocate_goods.py
@@ -1,26 +1,59 @@
 from model import Product, WareHouse
 
-def create_product_and_warehouse(
-        warehouse_name, product_article,
-        product_name, qty
-):
-    return (
-        WareHouse(warehouse_name),
-        Product(product_article, product_name, qty)
-    )
 
-def test_allocate_product_to_warehouse():
-    warehouse, product = create_product_and_warehouse(
-        'warehouse',
-        'product-1', 'Table', 5
-    )
-    warehouse.allocate(product)
-    assert warehouse.available_qty == 5
+# def test_allocate_product_to_warehouse():
+#     warehouse, product = create_product_and_warehouse(
+#         'warehouse',
+#         'product-1', 'Table', 5
+#     )
+#     warehouse.allocate(product)
+#     assert warehouse.available_qty == 5
+#
+# def test_deallocate_product_to_warehouse():
+#     warehouse, product = create_product_and_warehouse(
+#         'warehouse',
+#         'product-1', 'Table', 2
+#     )
+#     warehouse.deallocate(product)
+#     assert warehouse.available_qty == 3
 
-def test_deallocate_product_to_warehouse():
-    warehouse, product = create_product_and_warehouse(
-        'warehouse',
-        'product-1', 'Table', 2
-    )
-    warehouse.deallocate(product)
-    assert warehouse.available_qty == 3
+def test_if_exist_product_in_catalog():
+    catalog = WareHouse('warehouse')
+    product = Product('product', 'TABLE', 20)
+
+    catalog.add_product(product)
+    assert catalog.has_product_in_catalog(product)
+
+def test_can_add_qty_of_product():
+    catalog = WareHouse('warehouse')
+    product = Product('product', 'TABLE', 20)
+
+    catalog.add_product(product)
+    assert catalog.products[product.article]['qty'] == 20
+
+def test_can_add_many_times_qty_of_product():
+    catalog = WareHouse('warehouse')
+    product = Product('product', 'TABLE', 20)
+    product1 = Product('product', 'TABLE', 50)
+
+    catalog.add_product(product)
+    catalog.add_product(product1)
+    assert catalog.products[product.article]['qty'] == 70
+
+def test_subtract_qty_of_product_from_warehouse():
+    catalog = WareHouse('warehouse')
+    product = Product('product', 'TABLE', 20)
+    catalog.add_product(product)
+
+    product1 = Product('product', 'TABLE', 13)
+    catalog.subtract_product(product1)
+    assert catalog.products[product.article]['qty'] == 7
+
+def test_can_subtract_qty_of_product_from_warehouse():
+    catalog = WareHouse('warehouse')
+    product = Product('product', 'TABLE', 20)
+    catalog.add_product(product)
+
+    product1 = Product('product', 'TABLE', 25)
+    catalog.subtract_product(product1)
+    assert catalog.can_subtract(product1) is False
